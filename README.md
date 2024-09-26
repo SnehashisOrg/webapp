d181a2e87ddb408c05f993a05c6dbf8a63b3fe49
Snehashis Lenka <slenka321@gmail.com>
2024-09-26 13:30:51 -0400

    README and app updates

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # CSYE6225: Network Structures & Cloud Computing
 
 ## Assignment 01
+
+### Assignment Stack
+<b>1. Server:</b> <i>Ubuntu 24.04 LTS</i>
+<b>2. Programming Language:</b> <i>Python</i>
+<b>3. Database:</b> <i>MySQL</i>
+<b>4. ORM:</b> <i>SQLAlchemy</i>
+<b>5. UI Framework:</b> <i>N/A</i>
+<b>6. CSS:</b> <i>N/A</i>
+
+#### Prerequisites to run:
+
+<i>1. Python should be installed in your system</i>
+<i>2. MySQL Community Server should be installed, after that you can download MySQL Workbench ease of accessing different MySQL resources</i>
+<i>3. You can install Postman so as to test the API endpoints seamlessly</i>
+<i>4. Should have git installed in your system</i>
+
+#### Steps to run this locally:
+<i>1. Clone this repo in your local, use the below commands in your terminal:</i>
+```
+git clone <github_repo_link>
+cd <github_repo>
+```
+
+<i>2. Run the below command in the terminal to install the python packages:</i>
+```
+pip install -r requirements.txt
+```
+
+<i>3. Create a .env file in the repo, and add the values for database credentials</i>
+
+<i>4. Run the app.py file, uvicorn will run the FastAPI server</i>
+```
+python app.py
+```
+
+<i>5. Use Postman to validate the API endpoints.</i>
+
+<i>6. To check for the status <b>503: Service Unavailable</b>, stop the MySQL server instance and hit the API from Postman</i>
--- app.py
+++ app.py
@@ -12,22 +12,32 @@ load_dotenv()
 # the main entrypoint to use FastAPI.
 app = FastAPI()
 
+# setting up the logger 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Database credentials
 USER = os.getenv("MYSQL_USER")
 PASSWORD = os.getenv("MYSQL_PASSWORD")
 HOST = os.getenv("MYSQL_HOST")
 PORT = os.getenv("MYSQL_PORT")
 DATABASE = os.getenv("MYSQL_DATABASE")
 
+# setting up the headers
 HEADERS = {
         "Cache-Control": 'no-cache, no-store, must-revalidate;',
         "Pragma": 'no-cache',
         "X-Content-Type-Options": 'nosniff'
     }
 
+'''
+Function: get_database_connection()
+Desc.: Checks the database connection
+Params: None
+Return: bool
+'''
 def get_database_connection() -> bool:
+    # connection string
     CONNECTION_STRING = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
     engine = create_engine(CONNECTION_STRING)
@@ -40,11 +50,16 @@ def get_database_connection() -> bool:
         logger.info(f"Error in database connection!  {e}")
         return False
 
+'''
+API Endpoints
+'''
 @app.get("/healthz")
 async def healthcheck(request: Request, response: Response):
+    # checks for the scenarios when there's a body or query params in the request
     if await request.body() or request.query_params:
-        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, headers=HEADERS)
     
+    # checks if the database connection is up
     if not get_database_connection():
         return Response(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, headers=HEADERS)
     
@@ -57,7 +72,9 @@ async def healthcheck(request: Request, response: Response):
 @app.head("/healthz")
 @app.options("/healthz")
 def healthcheck():
+    # checks for the scenarios when the method type is not GET
     return Response(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, headers=HEADERS)
-    
+
+# Code entrypoint
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
