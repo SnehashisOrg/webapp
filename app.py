cfccf90c36a9cae035d4b9b91c87613c26ef73dc
Snehashis Lenka <slenka321@gmail.com>
2024-12-06 19:24:28 -0500

    Code review

--- app.py
+++ app.py
@@ -212,22 +212,22 @@ async def healthcheck(request: Request, response: Response):
 
 
 """
-GET: /cicd 
+GET: /cicd_new 
 cicd endpoint - To check instance refresh
 """
-@app.get("/cicd")
+@app.get("/cicd_new")
 async def cicd(request: Request, response: Response):
     # checks for the scenarios when there's a body or query params in the request
     if await request.body() or request.query_params:
-        logger.info("/cicd: query params not allowed...")
+        logger.info("/cicd_new: query params not allowed...")
         return Response(status_code=status.HTTP_400_BAD_REQUEST, headers=HEADERS)
     
     # checks if the database connection is up
     if not get_database_connection():
-        logger.info("/cicd: database is not up yet...")
+        logger.info("/cicd_new: database is not up yet...")
         return Response(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, headers=HEADERS)
     
-    logger.info("/cicd: the database is up and in service...")
+    logger.info("/cicd_new: the database is up and in service...")
     return Response(status_code=status.HTTP_200_OK, headers=HEADERS)
 """
 /healthz
